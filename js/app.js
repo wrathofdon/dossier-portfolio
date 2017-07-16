37e7eeafcf40488b107749205d091c31da7bf752
la januari <lajanuari@gmail.com>
2017-07-15 21:17:41 -0700

    oops time to switch to the weekend branch

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var pixArray = [];
+var lastPix = [];
+
+//constructor
+function Display(filePath, petNameSpeciesBreed, people, costume, owner){
+  this.filePath = filePath;
+  this.name = name;
+  this.petNameSpeciesBreed = [];
+  this.people = [];
+  this.costume = costume;
+  this.owner = owner;
+  pixArray.push(this);
+}
+
+new Display ('bailey.jpg','Bailey', 'ShiTzuDoodle', 'Savannah', false, 'Linda');
+new Display ('baileyBed.jpg', 'Bailey', 'ShiTzuDoodle', false, false, 'Linda');
+new Display ('baileyDance.jpg', 'Bailey', 'ShiTzuDoodle', 'Savannah', false, 'Linda');
+new Display ('bails.jpg');
+new Display ('belind.jpg' );
+new Display ('belleIndieSteve.jpg' );
+new Display ('biscuit.jpg' );
+new Display ('boston_friends.jpg' );
+new Display ('bostonHomecoming' );
+new Display ('bosty.jpg' );
+new Display ('bostybath.jpg' );
+new Display ('chillinlulu.jpg' );
+new Display ('gertrude.jpg' );
+new Display ('halloween.jpg' );
+new Display ('highFlying.jpg' );
+new Display ('hildeMovesIn.jpg' );
+new Display ('hotdog.jpg' );
+new Display ('indiebelle.jpg' );
+new Display ('instafriends.jpg' );
+new Display ('lulu.jpg' );
+new Display ('luluMagoo.jpg' );
+new Display ('' );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
+new Display ( );
